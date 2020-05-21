00ab0f94f207e085144d435e8f2491f6ba304a84
chadcarlson <chad.carlson@platform.sh>
2020-05-21 09:35:04 -0400

    Update to latest upstream

--- backend/build.sh
+++ backend/build.sh
@@ -13,7 +13,8 @@ rm config/environments/development/database.json && mv platformsh/database.js co
 rm config/environments/development/server.json && mv platformsh/server.json config/environments/development/server.json
 
 # Move index.html with working admin link.
-rm public/index.html && mv platformsh/index.html public/index.html
+mv public/index.html template.html
+mv platformsh/index.html public/index.html
 
 # Rebuild the admin panel.
 yarn build
