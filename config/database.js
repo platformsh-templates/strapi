a4d60dfbc9f10065c97e30d4d172d72e2b48ed60
hacktivist123 <akintayoshedrack@gmail.com>
2021-08-03 23:42:42 +0100

    test mysql

--- config/database.js
+++ config/database.js
@@ -1,13 +1,88 @@
+// const config = require("platformsh-config").config();
+
+// // Uncomment the line below if you would like to use a Postgres Database
+// // let dbRelationshipPostgres = "postgresdatabase";
+
+// // // Uncomment the line below if you would like to use a MongoDB Database
+// let dbRelationshipMongo = "mongodatabase";
+
+// // Uncomment the line below if you would like to use a MySQL Database
+// // let dbRelationshipMySql = "dbmysql"
+
+// // Strapi default sqlite settings.
+// let settings = {
+//   client: "sqlite",
+//   filename: process.env.DATABASE_FILENAME || ".tmp/data.db",
+// };
+
+// let options = {
+//   useNullAsDefault: true,
+// };
+
+// if (config.isValidPlatform() && !config.inBuild()) {
+//   // Platform.sh database configuration.
+//   const credentials = config.credentials(dbRelationshipMongo);
+
+//   console.log(
+//     `Using Platform.sh configuration with relationship ${dbRelationshipMongo}.`
+//   );
+
+//   var mongoSettings = {
+//     client: "mongo",
+//     host: credentials.ip,
+//     port: credentials.port,
+//     database: credentials.path,
+//     username: credentials.username,
+//     password: credentials.password,
+//   };
+
+//   var mongoOptions = {
+//     ssl: true,
+//     // debug: false,
+//     // acquireConnectionTimeout: 100000,
+//     // pool: {
+//     //   min: 0,
+//     //   max: 10,
+//     //   createTimeoutMillis: 30000,
+//     //   acquireTimeoutMillis: 600000,
+//     //   idleTimeoutMillis: 20000,
+//     //   reapIntervalMillis: 20000,
+//     //   createRetryIntervalMillis: 200,
+//     // },
+//   };
+// } else {
+//   if (config.isValidPlatform()) {
+//     // Build hook configuration message.
+//     console.log(
+//       "Using default configuration during Platform.sh build hook until relationships are available."
+//     );
+//   } else {
+//     // Strapi default local configuration.
+//     console.log(
+//       "Not in a Platform.sh Environment. Using default local sqlite configuration."
+//     );
+//   }
+// }
+
+// module.exports = {
+//   defaultConnection: "mongodb",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: settings,
+//       options: options,
+//     },
+//     mongodb: {
+//       connector: "mongoose",
+//       settings: mongoSettings,
+//       options: mongoOptions,
+//     },
+//   },
+// };
+
 const config = require("platformsh-config").config();
 
-// Uncomment the line below if you would like to use a Postgres Database
-// let dbRelationshipPostgres = "postgresdatabase";
-
-// // Uncomment the line below if you would like to use a MongoDB Database
-let dbRelationshipMongo = "mongodatabase";
-
-// Uncomment the line below if you would like to use a MySQL Database
-// let dbRelationshipMySql = "dbmysql"
+let dbRelationshipMySql = "mysqldatabase";
 
 // Strapi default sqlite settings.
 let settings = {
@@ -21,14 +96,14 @@ let options = {
 
 if (config.isValidPlatform() && !config.inBuild()) {
   // Platform.sh database configuration.
-  const credentials = config.credentials(dbRelationshipMongo);
+  const credentials = config.credentials(dbRelationshipMySql);
 
   console.log(
-    `Using Platform.sh configuration with relationship ${dbRelationshipMongo}.`
+    `Using Platform.sh configuration with relationship ${dbRelationshipMySql}.`
   );
 
-  var mongoSettings = {
-    client: "mongo",
+  settings = {
+    client: "mysql",
     host: credentials.ip,
     port: credentials.port,
     database: credentials.path,
@@ -36,19 +111,19 @@ if (config.isValidPlatform() && !config.inBuild()) {
     password: credentials.password,
   };
 
-  var mongoOptions = {
-    ssl: true,
-    // debug: false,
-    // acquireConnectionTimeout: 100000,
-    // pool: {
-    //   min: 0,
-    //   max: 10,
-    //   createTimeoutMillis: 30000,
-    //   acquireTimeoutMillis: 600000,
-    //   idleTimeoutMillis: 20000,
-    //   reapIntervalMillis: 20000,
-    //   createRetryIntervalMillis: 200,
-    // },
+  options = {
+    ssl: false,
+    debug: false,
+    acquireConnectionTimeout: 100000,
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 30000,
+      acquireTimeoutMillis: 600000,
+      idleTimeoutMillis: 20000,
+      reapIntervalMillis: 20000,
+      createRetryIntervalMillis: 200,
+    },
   };
 } else {
   if (config.isValidPlatform()) {
@@ -65,17 +140,12 @@ if (config.isValidPlatform() && !config.inBuild()) {
 }
 
 module.exports = {
-  defaultConnection: "mongodb",
+  defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: settings,
       options: options,
-    },
-    mongodb: {
-      connector: "mongoose",
-      settings: mongoSettings,
-      options: mongoOptions,
     },
   },
 };
