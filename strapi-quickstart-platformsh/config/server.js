2f5dad936bf78147e5cff13acee0f1b756d1fa1b
Shedrack akintayo <akintayoshedrack@gmail.com>
2021-08-09 17:00:21 +0100

    Update to latest upstream

---
+++ strapi-quickstart-platformsh/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '03dc0ed0a5378bca84cc6e264058f003'),
+    },
+  },
+});
