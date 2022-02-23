47a31f9b2e4fcafcb6ab094537a5a3816bdbaf4a
Platform.sh DevRel Team <devrel@internal.platform.sh>
2022-02-23 16:43:58 +0000

    scheduled updates.

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '2c2a10ef48c5ef327b1193c4d16523f7'),
+    secret: env('ADMIN_JWT_SECRET', 'ee31fccbab179547a9763ab07f2ec97b'),
   },
 });
